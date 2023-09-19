b6270d72cabc01fcbd3196518da60649698513a5
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-09-19 18:27:00 -0400

    0-3

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -4,28 +4,26 @@
 
 (* ****** ****** *)
 
-let concat (s1: string) (s2: string): string =
-  let l1 = String.length s1 in
-  let l2 = String.length s2 in
-  let s = String.init (l1 + l2) (fun i ->
-    if i < l1 then string_get(s1, i) else string_get(s2, (i - l1))
-  ) in
-  s;;
+(*num_length function returns the number of digits in a number*)
+let rec num_length(x: int): int = 
+  if x < 10 then 1 else 1 + num_length(x/10)
 
-  let int2str(i0: int): string =
-    if i0 = 0 then
-        "0"
+(*int2str function converts int to a string*)
+let int2str(i0: int): string =
+  (*get_digit returns the individual digit at specified index of number*)
+  let rec get_digit(x: int): int =
+    if x = 0 then 1 else 10 * get_digit (x-1)
+  in
+
+  let length = num_length (abs i0) in
+  let is_neg = i0 < 0 in
+  string_init (length + (if is_neg then 1 else 0))
+  (fun i -> 
+    if i = 0 && is_neg then '-'
     else
-        let rec loop x s = 
-            if x = 0 then
-                s
-            else
-                let d = x mod 10 in 
-                let c = chr(48 + d) in
-                let s1 = str(c) in
-                loop (x / 10) (concat s1 s)
-        in
-        if i0 < 0 then
-            concat "-" (loop (-i0) "")
-        else
-            loop i0 "";;
+      (*dividing i0 to get different segments of the number and getting last digit*)
+      let index = length - (if is_neg then (i - 1) else i) - 1 in
+      let digit = abs (i0 / get_digit index) mod 10
+    in
+    chr(ord '0' + digit)
+    );;
