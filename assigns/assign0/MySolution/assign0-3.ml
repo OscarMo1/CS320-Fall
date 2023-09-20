7729ddbb4e0abdb38006d951ea16fae49fe41d8a
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-09-20 15:30:43 -0400

    1-2

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -11,8 +11,8 @@ let rec num_length(x: int): int =
 
 let int2str(i0: int): string =
   (*get_digit returns the individual digit at specified index of number*)
-  let rec get_digit(x: int): int =
-    if x = 0 then 1 else 10 * get_digit (x-1)
+  let rec getDigit(x: int): int =
+    if x = 0 then 1 else 10 * getDigit (x-1)
   in
 
   let length = num_length (abs i0) in
@@ -21,9 +21,8 @@ let int2str(i0: int): string =
   (fun i -> 
     if i = 0 && isNeg then '-'
     else
-      (*dividing i0 to get different segments of the number and getting last digit*)
       let index = length - (if isNeg then (i - 1) else i) - 1 in
-      let digit = abs (i0 / get_digit index) mod 10
+      let digit = abs (i0 / getDigit index) mod 10
     in
     chr(ord '0' + digit)
     );;
--- assigns/assign1/MySolution/assign1-2.ml
+++ assigns/assign1/MySolution/assign1-2.ml
@@ -1,13 +1,15 @@
-(* ****** ****** *)
-#use "./../MySolution/assign1-2.ml";;
-(* ****** ****** *)
+
+#use "./../../../classlib/OCaml/MyOCaml.ml"
+
 
 let string_merge(cs1: string)(cs2: string): string =
   let merged = string_make_fwork (fun work ->
+    let strlen1 = string_length cs1 in
+    let strlen2 = string_length cs2 in
     let rec merge_strings i1 i2 =
-      if i1 < String.length cs1 && i2 < String.length cs2 then (
-        let c1 = String.get cs1 i1 in
-        let c2 = String.get cs2 i2 in
+      if i1 < strlen1 && i2 < strlen2 then (
+        let c1 = string_get_at cs1 i1 in
+        let c2 = string_get_at cs2 i2 in
         if c1 <= c2 then (
           work c1;
           merge_strings (i1 + 1) i2
@@ -15,17 +17,54 @@ let string_merge(cs1: string)(cs2: string): string =
           work c2;
           merge_strings i1 (i2 + 1)
         )
-      ) else if i1 < String.length cs1 then (
-        work (String.get cs1 i1);
-        merge_strings (i1 + 1) i2
-      ) else if i2 < String.length cs2 then (
-        work (String.get cs2 i2);
-        merge_strings i1 (i2 + 1)
-      ) else ()
+      ) else if i1 < strlen1 then (
+        let rec merge_remaining_characters i1 i2 work cs1 =
+          if i1 < string_length cs1 then (
+            work (string_get_at cs1 i1);
+            merge_remaining_characters (i1 + 1) i2 work cs1
+          )
+          else if i2 < string_length cs2 then (
+            for i = i2 to string_length cs2 - 1 do
+              work (string_get_at cs2 i);
+            done
+          )
+        in
+        
+        merge_remaining_characters i1 i2 work cs1;
+        
+      ) else if i2 < strlen2 then (
+        let rec merge_remaining_characters i1 i2 work cs1 cs2 =
+          if i1 < string_length cs1 && i2 < string_length cs2 then (
+            let c1 = string_get_at cs1 i1 in
+            let c2 = string_get_at cs2 i2 in
+            if c1 <= c2 then (
+              work c1;
+              merge_remaining_characters (i1 + 1) i2 work cs1 cs2
+            ) else (
+              work c2;
+              merge_remaining_characters i1 (i2 + 1) work cs1 cs2
+            )
+          )
+          else if i1 < string_length cs1 then (
+            for i = i1 to string_length cs1 - 1 do
+              work (string_get_at cs1 i);
+            done
+          )
+          else if i2 < string_length cs2 then (
+            for i = i2 to string_length cs2 - 1 do
+              work (string_get_at cs2 i);
+            done
+          )
+        in
+        
+        merge_remaining_characters i1 i2 work cs1 cs2;
+        
+      )  
     in
     merge_strings 0 0
   ) in
   merged
 ;;
+
 
 
