3866a1ee76f18297905ce98c622e07dc1d5b778a
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-09-26 11:48:57 -0400

    error fix

--- assigns/assign2/MySolution/OCaml/assign2-2.ml
+++ assigns/assign2/MySolution/OCaml/assign2-2.ml
@@ -1,22 +1,10 @@
 #use "./../../assign2.ml";;
-
-let rec mylist_length(xs: 'a mylist): int =
-  match xs with
-  | MyNil -> 0  (* An empty list has a length of 0. *)
-  | MyCons(_, xs') | MySnoc(xs', _) | MyReverse(xs') ->
-    (* Recursively calculate the length of the tail of the list and add 1. *)
-    1 + mylist_length(xs')
-  | MyAppend2(xs1, xs2) ->
-    (* Handle the MyAppend2 constructor with two sublists. *)
-    mylist_length(xs1) + mylist_length(xs2)
-;;
-
 let rec mylist_get_at(xs: 'a mylist)(i0: int): 'a =
   match (xs, i0) with
   | (MyNil, _) -> mylist_subscript_exn ()  (* Out of bounds for an empty list. *)
   | (_, 0) -> (
       match xs with
-      | MyCons(x, _) -> x  (* Found the element at position 0. *)
+      | MyCons(x, _) | MySnoc(_, x) -> x  (* Found the element at position 0. *)
       | _ -> mylist_subscript_exn ()  (* Unexpected case. Should not happen. *)
     )
   | (_, _) when i0 < 0 -> mylist_subscript_exn ()  (* Negative index is out of bounds. *)
@@ -30,5 +18,12 @@ let rec mylist_get_at(xs: 'a mylist)(i0: int): 'a =
     else
       mylist_get_at(xs2, i0 - len_xs1)  (* The element is in the second sublist. *)
 ;;
+
+(* Example usage:
+let myList = MyCons(1, MyCons(2, MyCons(3, MyNil)))
+let element = mylist_get_at myList 1  (* This will be 2. *)
+*)
+
+
 
 
