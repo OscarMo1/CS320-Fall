840af7ba85ea295574fa7609b0acb2a5c9924604
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-09-26 11:42:15 -0400

    error fix

--- assigns/assign2/MySolution/OCaml/assign2-1.ml
+++ assigns/assign2/MySolution/OCaml/assign2-1.ml
@@ -1,4 +1,14 @@
-(* ****** ****** *)
-#use "./../../MySolution/OCaml/assign2-1.ml";;
-(* ****** ****** *)
+
+#use "./../../assign2.ml";;
+
+let rec mylist_length(xs: 'a mylist): int =
+  match xs with
+  | MyNil -> 0  (* An empty list has a length of 0. *)
+  | MyCons(_, xs') | MySnoc(xs', _) | MyReverse(xs') ->
+    (* Recursively calculate the length of the tail of the list and add 1. *)
+    1 + mylist_length(xs')
+  | MyAppend2(xs1, xs2) ->
+    (* Handle the MyAppend2 constructor with two sublists. *)
+    mylist_length(xs1) + mylist_length(xs2)
+;;
 
