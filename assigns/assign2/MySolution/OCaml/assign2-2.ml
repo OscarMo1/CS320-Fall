b97f32f05d7ed47e3790ac0f814d50e562c2166a
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-09-27 18:40:32 -0400

    edited 2-5.py and 2-4.ml

--- assigns/assign2/MySolution/OCaml/assign2-2.ml
+++ assigns/assign2/MySolution/OCaml/assign2-2.ml
@@ -1,16 +1,20 @@
 #use "./../../assign2.ml";;
 
 
+
 let rec mylist_length(xs: 'a mylist): int =
   match xs with
   | MyNil -> 0  (* An empty list has a length of 0. *)
-  | MyCons(_, xs') | MySnoc(xs', _) | MyReverse(xs') ->
+  | MyCons(_, xs )| MySnoc(xs, _) ->
+    1 + mylist_length(xs)
+  | MyReverse(xs) ->
     (* Recursively calculate the length of the tail of the list and add 1. *)
-    1 + mylist_length(xs')
+    mylist_length(xs)
   | MyAppend2(xs1, xs2) ->
     (* Handle the MyAppend2 constructor with two sublists. *)
     mylist_length(xs1) + mylist_length(xs2)
 ;;
+
 
 let rec mylist_get_at (xs: 'a mylist) (i0: int): 'a =
   match (xs, i0) with
--- assigns/assign2/MySolution/OCaml/assign2-3.ml
+++ assigns/assign2/MySolution/OCaml/assign2-3.ml
@@ -1,8 +1,7 @@
 #use "./../../assign2.ml";;
-type ('xs, 'x0) iforeach = 'xs -> ('x0 -> unit) -> unit
 
-  let foldleft_to_iforeach
-    (foldleft: ('xs, 'x0, int) foldleft): ('xs, 'x0) iforeach =
-    fun xs work ->
-      let _ = foldleft xs 0 (fun _ x -> work x) in ()
+let foldleft_to_iforeach (foldleft: ('xs, 'x0, 'r0) foldleft): ('xs, 'x0) iforeach =
+  
+    ;;
+
   
