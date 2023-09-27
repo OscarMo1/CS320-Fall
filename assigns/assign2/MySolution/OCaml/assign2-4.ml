a9d67d1e56ec327e619dc402fdaaa38c0a258f02
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-09-27 17:43:58 -0400

    edited 2-5.py and 2-4.ml

--- assigns/assign2/MySolution/OCaml/assign2-4.ml
+++ assigns/assign2/MySolution/OCaml/assign2-4.ml
@@ -1,4 +1,6 @@
 #use "./../../assign2.ml";;
+#use "./../../../../classlib/OCaml/MyOCaml.ml"
+
 let string_sepjoin_list (sep: string) (xs: string list): string =
-  let combine = list_foldright xs "" (fun s1 s2 -> if s2 = "" then s1 else s1 ^ sep ^ s2) in
+  let combine = list_foldleft xs "" (fun s1 s2 -> if s2 = "" then s1 else string_appe) in
   combine
