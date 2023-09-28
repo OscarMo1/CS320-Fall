cc3e1997d3031e5abc9ba779e3d03c576460beef
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-09-27 23:05:45 -0400

    2-2

--- assigns/assign2/MySolution/OCaml/assign2-2.ml
+++ assigns/assign2/MySolution/OCaml/assign2-2.ml
@@ -1,35 +1,40 @@
 #use "./../../assign2.ml";;
 
-type 'a mylist =
-  | MyNil
-  | MyCons of 'a * 'a mylist
-  | MySnoc of 'a mylist * 'a
-  | MyReverse of 'a mylist
-  | MyAppend2 of 'a mylist * 'a mylist
-
-exception MyListSubscriptExn
-
-let rec mylist_length (xs: 'a mylist): int =
+let rec mylist_length(xs: 'a mylist): int =
   match xs with
-  | MyNil -> 0
-  | MyCons (_, t) | MySnoc (t, _) -> 1 + mylist_length t
-  | MyReverse xs' -> mylist_length xs'
-  | MyAppend2 (xs1, xs2) -> mylist_length xs1 + mylist_length xs2
+  | MyNil -> 0  (* An empty list has a length of 0. *)
+  | MyCons(_, xs )| MySnoc(xs, _) ->
+    1 + mylist_length(xs)
+  | MyReverse(xs) ->
+    (* Recursively calculate the length of the tail of the list and add 1. *)
+    mylist_length(xs)
+  | MyAppend2(xs1, xs2) ->
+    (* Handle the MyAppend2 constructor with two sublists. *)
+    mylist_length(xs1) + mylist_length(xs2)
+;;
+
 
 let rec mylist_get_at (xs: 'a mylist) (i0: int): 'a =
-  if i0 < 0 then raise MyListSubscriptExn
-  else
+  let rec get_at_helper i xs =
     match xs with
-    | MyNil -> raise MyListSubscriptExn
-    | MyCons (h, t) ->
-      if i0 = 0 then h else mylist_get_at t (i0 - 1)
-    | MySnoc (t, h) ->
-      let len = mylist_length t in
-      if i0 = len then h
-      else if i0 < len then mylist_get_at t i0
-      else raise MyListSubscriptExn
-    | MyReverse xs' -> mylist_get_at xs' (mylist_length xs' - 1 - i0)
-    | MyAppend2 (xs1, xs2) ->
-      let l1 = mylist_length xs1 in
-      if i0 < l1 then mylist_get_at xs1 i0
-      else mylist_get_at xs2 (i0 - l1)
+    | MyCons (x, _) when i = 0 -> x
+    | MyCons (_, rest) when i > 0 -> get_at_helper (i - 1) rest
+    | MySnoc (_, x) when i = 0 -> x
+    | MySnoc (rest, _) when i > 0 -> get_at_helper (i - 1) rest
+    | MyAppend2 (xs1, xs2) when i >= 0 ->
+      let xs1_len = mylist_length xs1 in
+      if i < xs1_len then
+        get_at_helper i xs1
+      else
+        get_at_helper (i - xs1_len) xs2
+    | MyReverse xs when i >= 0 ->
+      let xs_len = mylist_length xs in
+      if i < xs_len then
+        get_at_helper(xs_len - 1 - i) xs
+      else
+        mylist_subscript_exn ()
+    | _ -> mylist_subscript_exn ()
+  in
+  if i0 < 0 then mylist_subscript_exn ()
+  else get_at_helper i0 xs
+
