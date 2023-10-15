dbc23f708ee44e6e00408ca2c5c391dec6ac79d9
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-10-14 21:56:44 -0400

    updating

--- assigns/assign4/MySolution/OCaml/assign4-2.ml
+++ assigns/assign4/MySolution/OCaml/assign4-2.ml
@@ -1,11 +1,5 @@
 #use "./../../../../classlib/OCaml/MyOCaml.ml"
 
-type 'a strcon =
-  | StrNil
-  | StrCons of 'a * (unit -> 'a strcon)
-
-type 'a stream = unit -> 'a strcon
-
 let rec nat_pairs_stream i j () =
   StrCons((i, j), fun () -> nat_pairs_stream i (j + 1) ())
 ;;
--- assigns/assign4/MySolution/OCaml/assign4-3.ml
+++ assigns/assign4/MySolution/OCaml/assign4-3.ml
@@ -1,1 +1,33 @@
 #use "./../../../../classlib/OCaml/MyOCaml.ml"
+
+let rec
+gtree_streamize_dfs (xs: 'a gtree): 'a list =
+  let rec dfs_streamize_helper tree =
+    match tree with
+    | GTnil -> []
+    | GTcons (value, children) ->
+      let children_list = list_concat_map children dfs_streamize_helper in
+      value :: children_list
+  in dfs_streamize_helper xs
+
+and
+list_concat_map xs f =
+  let rec concat_map_helper xs f acc =
+    match xs with
+    | [] -> acc
+    | x :: xs' ->
+      let x_list = f x in
+      let acc' = acc @ x_list in
+      concat_map_helper xs' f acc'
+  in concat_map_helper xs f []
+
+let rec
+gtree_streamize_bfs (xs: 'a gtree): 'a list =
+  let rec bfs_streamize_helper trees =
+    match trees with
+    | [] -> []
+    | GTnil :: rest -> bfs_streamize_helper rest
+    | GTcons (value, children) :: rest ->
+      let children_list = list_concat children in
+      value :: children_list @ bfs_streamize_helper (rest @ children)
+  in bfs_streamize_helper [xs]
