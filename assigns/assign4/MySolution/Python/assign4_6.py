cb0d5c547d7848917a1c02f341f948378f2701e2
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-10-16 19:30:02 -0400

    4-3

--- assigns/assign4/MySolution/Python/assign4_6.py
+++ assigns/assign4/MySolution/Python/assign4_6.py
@@ -1,36 +1,13 @@
-def generator_of_stream(fxs):
-    while True:
-        cxs = fxs()
-        if cxs[0] == 0:
-            break
-        else:
-            fxs = cxs[1]
-            yield cxs[2]
-    raise StopIteration
-
-def theNatPairs_cubesum():
-    def cube_sum(i, j):
-        return i**3 + j**3
-
-    def pairs_generator():
+def the_nat_pairs_cubesum():
+    def cubesum_gen():
         i, j = 0, 0
         while True:
-            yield (i, j)
-            if j == 0:
+            cubesum_i = i ** 3
+            cubesum_j = j ** 3
+            if cubesum_i + cubesum_j < cubesum_gen():
+                yield (i, j)
                 i += 1
-                j = i
             else:
-                j -= 1
+                j += 1
 
-    def compare_pairs(pair1, pair2):
-        i1, j1 = pair1
-        i2, j2 = pair2
-        return cube_sum(i1, j1) < cube_sum(i2, j2)
-
-    sorted_pairs = sorted(pairs_generator(), key=lambda pair: cube_sum(pair[0], pair[1]))
-
-    for pair in sorted_pairs:
-        yield pair
-
-
-
+    return cubesum_gen()
