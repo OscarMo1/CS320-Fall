f0935d80f64c931149fe9ae3ca567dd26b5dfae7
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-11-06 23:30:35 -0500

    hw5

--- assigns/assign5/MySolution/OCaml/assign5.ml
+++ assigns/assign5/MySolution/OCaml/assign5.ml
@@ -1,107 +1,57 @@
 #use "./../../../../classlib/OCaml/MyOCaml.ml";;
-(*
 
-Grammar (<expr> is the start symbol)
-
-<digit> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
-<num>   ::= <digit> | <digit><num>
-<expr>  ::= <num> 
-          | (add <exprs> )
-          | (mul <exprs> )
-<exprs> ::= <expr> | <expr><exprs>
-
-*)
 
 type expr =
-  | Int of int       (* 1, 2, 3, 4 ...  *)
-  | Add of expr list (* (add e1 e2 ...) *)
-  | Mul of expr list (* (mul e1 e2 ...) *)
-
-(* turn a string into a list of chars *)
-let string_listize (s : string) : char list =
-  list_make_fwork(fun work -> string_foreach s work)
-
-(* remove blank chars at the front of a list *)
-let rec trim cs =
-  match cs with
-  | [] -> cs
-  | '\n' :: cs -> trim cs
-  | '\t' :: cs -> trim cs
-  | '\r' :: cs -> trim cs
-  | ' ' :: cs -> trim cs
-  | _ -> cs
-
-(* Please implement a parse function. When given a valid string according
-   to the grammar, your parse function returns an expr value encoding the
-   expression.
-
-   Example (Accpeted Strings):
-   parse "(add 1 2 3)" = Some (Add [Int 1; Int 2; Int 3])
-   parse "(mul (add 1 2) 3 (mul 1))" = Some (Mul [Add [Int 1; Int 2]; Int 3; Mul [Int 1]])
-
-   Example (Rejected Strings):
-   parse "()" = None
-   parse "(add)" = None
-   parse "(add 1 2))" = None
-   parse "((mul 1 2)" = None
-
-*)
+  | Int of int
+  | Add of expr list
+  | Mul of expr list
 
 let is_digit c = c >= '0' && c <= '9'
 
-let rec parse_num cs =
+let rec parse_num acc cs =
   match cs with
-  | [] -> None
-  | ' ' :: rest -> parse_num (trim rest)
-  | '(' :: _ -> None
-  | ')' :: _ -> None
   | c :: rest when is_digit c ->
-    let (digits, rest') = parse_digits (String.make 1 c) rest in
-    Some (Int (int_of_string digits), rest')
-  | _ -> None
-
-and parse_digits acc cs =
-  match cs with
-  | [] -> (acc, [])
-  | c :: rest when is_digit c -> parse_digits (acc ^ (String.make 1 c)) rest
+    let (digits, rest') = parse_num (acc ^ (String.make 1 c)) rest in
+    (digits, rest')
   | _ -> (acc, cs)
 
+let rec parse_expr cs =
+  match cs with
+  | [] -> (None, cs)
+  | ' ' :: rest -> parse_expr (trim rest)
+  | '(' :: rest ->
+    let (op, rest') = parse_op (trim rest) in
+    (op, rest')
+  | _ -> parse_num "" cs
 
-and parse_expr (cs : char list) : expr option * char list =
-  match trim cs with
-  | ('0'..'9' as d) :: rest ->
-    let num, rest' = parse_num (d::rest) in
-    (Some (Int num), rest')
-  | '(' :: 'a' :: 'd' :: 'd' :: rest ->
-    let (exprs, rest') = parse_exprs rest in
-    (match exprs with
-      | [] -> (None, rest')
-      | _ -> (Some (Add exprs), rest'))
-  | '(' :: 'm' :: 'u' :: 'l' :: rest ->
-    let (exprs, rest') = parse_exprs rest in
-    (match exprs with
-      | [] -> (None, rest')
-      | _ -> (Some (Mul exprs), rest'))
+and parse_op cs =
+  match cs with
+  | [] -> (None, cs)
+  | 'a' :: 'd' :: 'd' :: ' ' :: rest -> parse_add rest
+  | 'm' :: 'u' :: 'l' :: ' ' :: rest -> parse_mul rest
   | _ -> (None, cs)
 
-and parse_exprs (cs : char list) : expr list * char list =
-  match trim cs with
-  | ')' :: rest -> ([], rest)
-  | _ ->
-    let (expr, rest) = parse_expr cs in
-    (match expr with
-      | None -> ([], rest)
-      | Some e ->
-        let (exprs, rest') = parse_exprs rest in
-        (e :: exprs, rest'))
+and parse_add cs =
+  let (exprs, rest') = parse_exprs cs in
+  match rest' with
+  | ')' :: rest'' -> (Some (Add exprs), rest'')
+  | _ -> (None, cs)
+
+and parse_mul cs =
+  let (exprs, rest') = parse_exprs cs in
+  match rest' with
+  | ')' :: rest'' -> (Some (Mul exprs), rest'')
+  | _ -> (None, cs)
+
+and parse_exprs cs =
+  match parse_expr cs with
+  | (Some expr, rest) ->
+    let (exprs, rest') = parse_exprs rest in
+    (expr :: exprs, rest')
+  | _ -> ([], cs)
 
 let parse (s : string) : expr option =
-  let char_list = string_listize s in
-  let (expr, rest) = parse_expr char_list in
-  if List.length (trim rest) = 0 then
-    expr
-  else
-    None
-;;
-
-
+  let cs = string_listize s in
+  match parse_expr (trim cs) with
+  | (Some expr, []) -> Some expr
+  | _ -> None
