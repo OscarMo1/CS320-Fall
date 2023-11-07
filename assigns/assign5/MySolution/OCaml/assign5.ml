76aa41832e931ae19399ad58fceabc3b62e1705c
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-11-06 23:47:11 -0500

    hw5

--- assigns/assign5/MySolution/OCaml/assign5.ml
+++ assigns/assign5/MySolution/OCaml/assign5.ml
@@ -46,57 +46,55 @@ let rec trim cs =
    parse "((mul 1 2)" = None
 
 *)
-type expr =
-  | Int of int
-  | Add of expr list
-  | Mul of expr list
 
-let is_digit c = c >= '0' && c <= '9'
+let is_digit char = char >= '0' && char <= '9'
 
-let rec parse_num acc cs =
-  match cs with
-  | c :: rest when is_digit c ->
-    let (digits, rest') = parse_num (acc ^ (String.make 1 c)) rest in
-    (digits, rest')
-  | _ -> (acc, cs)
+let rec parse_num str =
+  match str with
+  | [] -> None
+  | ' ' :: rest -> parse_num (trim rest)
+  | '(' :: _ -> None
+  | ')' :: _ -> None
+  | char :: rest when is_digit char ->
+    let (digits, rest') = parse_digits (String.make 1 char) rest in
+    Some (Int (int_of_string digits), rest')
+  | _ -> None
 
-let rec parse_expr cs =
-  match cs with
-  | [] -> (None, cs)
-  | ' ' :: rest -> parse_expr (trim rest)
-  | '(' :: rest ->
-    let (op, rest') = parse_op (trim rest) in
-    (op, rest')
-  | _ -> parse_num "" cs
+and parse_digits acc str =
+  match str with
+  | [] -> (acc, [])
+  | char :: rest when is_digit char -> parse_digits (acc ^ (String.make 1 char)) rest
+  | _ -> (acc, str)
 
-and parse_op cs =
-  match cs with
-  | [] -> (None, cs)
-  | 'a' :: 'd' :: 'd' :: ' ' :: rest -> parse_add rest
-  | 'm' :: 'u' :: 'l' :: ' ' :: rest -> parse_mul rest
-  | _ -> (None, cs)
-
-and parse_add cs =
-  let (exprs, rest') = parse_exprs cs in
-  match rest' with
-  | ')' :: rest'' -> (Some (Add exprs), rest'')
-  | _ -> (None, cs)
-
-and parse_mul cs =
-  let (exprs, rest') = parse_exprs cs in
-  match rest' with
-  | ')' :: rest'' -> (Some (Mul exprs), rest'')
-  | _ -> (None, cs)
-
-and parse_exprs cs =
-  match parse_expr cs with
-  | (Some expr, rest) ->
+and parse_exprs str =
+  match parse_expr str with
+  | Some (expr, rest) ->
     let (exprs, rest') = parse_exprs rest in
     (expr :: exprs, rest')
-  | _ -> ([], cs)
+  | None -> ([], str)
+
+and parse_expr str =
+  match str with
+  | [] -> None
+  | ' ' :: rest -> parse_expr (trim rest)
+  | '(' :: 'a' :: 'd' :: 'd' :: ' ' :: rest ->
+    let (exprs, rest') = parse_exprs rest in
+    begin
+      match rest' with
+      | ')' :: rest'' -> Some (Add exprs, rest'')
+      | _ -> None
+    end
+  | '(' :: 'm' :: 'u' :: 'l' :: ' ' :: rest ->
+    let (exprs, rest') = parse_exprs rest in
+    begin
+      match rest' with
+      | ')' :: rest'' -> Some (Mul exprs, rest'')
+      | _ -> None
+    end
+  | _ -> parse_num str
 
 let parse (s : string) : expr option =
-  let cs = string_listize s in
-  match parse_expr (trim cs) with
-  | (Some expr, []) -> Some expr
+  let str = string_listize s in
+  match parse_expr (trim str) with
+  | Some (expr, []) -> Some expr
   | _ -> None
