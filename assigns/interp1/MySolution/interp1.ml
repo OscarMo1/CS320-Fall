5caf56d125536373e38b487a8f69a0333df43dbb
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-11-27 19:56:30 -0500

    adjusted

--- assigns/interp1/MySolution/interp1.ml
+++ assigns/interp1/MySolution/interp1.ml
@@ -83,137 +83,113 @@ let const_to_string const =
   | Unit _ -> "Unit"
        
 
-  let parse_op op keyword_str =
-   let* _ = keyword keyword_str in
-   let* _ = char ';' in
-   let* _ = whitespaces in
-   pure (op :: prog)
- 
- let parse_prog' prog =
-   parse_op (Push c) "Push" <*> parse_const () >>| fun (op, c) -> op :: prog
-   <|> parse_op Pop "Pop" >>| fun op -> op :: prog
-   <|> parse_op Trace "Trace" >>| fun op -> op :: prog
-   <|> parse_op Add "Add" >>| fun op -> op :: prog
-   <|> parse_op Sub "Sub" >>| fun op -> op :: prog
-   <|> parse_op Mul "Mul" >>| fun op -> op :: prog
-   <|> parse_op Div "Div" >>| fun op -> op :: prog
-   <|> parse_op And "And" >>| fun op -> op :: prog
-   <|> parse_op Or "Or" >>| fun op -> op :: prog
-   <|> parse_op Not "Not" >>| fun op -> op :: prog
-   <|> parse_op Lt "Lt" >>| fun op -> op :: prog
-   <|> parse_op Gt "Gt" >>| fun op -> op :: prog
-   <|> pure (list_reverse prog)
- 
-  
+let rec parse_prog(prog: com list) =
+	(
+  let* _ = whitespaces in
+  let* _ = keyword "Push" in
+	let* _ = whitespaces in
+	let* c = parse_const  () in
+	let* _ = char ';' in
+	let* _ = whitespaces in
+	parse_prog ((Push c) :: prog))
+	<|> 
+	(let* _ = keyword "Pop;" in
+	parse_prog (Pop :: prog))
+	<|>
+	(let* _ = keyword "Trace;" in
+	parse_prog (Trace :: prog))
+	<|>
+	(let* _ = keyword "Add;" in
+	parse_prog (Add :: prog))
+	<|>	
+	(let* _ = keyword "Sub;" in
+	parse_prog (Sub :: prog))
+	<|>	
+	(let* _ = keyword "Mul;" in
+	parse_prog (Mul :: prog))
+	<|>	
+	(let* _ = keyword "Div;" in
+	parse_prog (Div :: prog))
+	<|>	
+	(let* _ = keyword "And;" in
+	parse_prog (And :: prog))
+	<|>	
+	(let* _ = keyword "Or;" in
+	parse_prog (Or :: prog))
+	<|>	
+	(let* _ = keyword "Not;" in
+	parse_prog (Not :: prog))
+	<|>	
+	(let* _ = keyword "Lt;" in
+	parse_prog (Lt :: prog))
+	<|>	
+	(let* _ = keyword "Gt;" in
+	parse_prog (Gt :: prog))
+	<|>
+	pure(list_reverse(prog))
 
 let string_parse_c(p: 'a parser)(s: string) =
   p(string_listize(s))
 ;;
 
 let interp (s: string) = 
-	let rec evaluate(stack: const list)(trace: string list)(prog: com list) = 
-		match prog with 
-		| c :: rest -> 
-			(match c with 
-			| Push ct -> evaluate(ct :: stack)(trace)(rest)
-			| Pop -> (if array_len stack = 0 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| _ :: st -> evaluate(st)(trace)(rest)
-					| [] -> None
-				))
-			| Trace -> (if array_len stack = 0 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| top :: st -> evaluate((Unit ()) :: st)(const_to_string (top) :: trace)(rest)
-					| [] -> None
-				))
-			| Add -> (if array_len stack < 2 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| i :: j :: st -> 
-						(match i, j with
-						| (Int i, Int j) -> evaluate( Int (i + j) :: st)(trace)(rest)
-						| _, _ -> (evaluate(stack)("Panic" :: trace)([])))
-					| [] -> None
-				))
-			| Sub -> (if array_len stack < 2 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| i :: j :: st -> 
-						(match i, j with
-						| (Int i, Int j) -> evaluate( Int (i - j) :: st)(trace)(rest)
-						| _, _ -> (evaluate(stack)("Panic" :: trace)([])))
-					| [] -> None
-				))
-			| Mul -> (if array_len stack < 2 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| i :: j :: st -> 
-						(match i, j with
-						| (Int i, Int j) -> evaluate( Int (i * j) :: st)(trace)(rest)
-						| _, _ -> (evaluate(stack)("Panic" :: trace)([])))
-					| [] -> None
-				))
-			| Div -> (if array_len stack < 2 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| i :: j :: st -> 
-						(match i, j with
-						| (Int _, Int 0) -> (evaluate(stack)("Panic" :: trace)([]))
-						| (Int i, Int j) -> evaluate( Int (i / j) :: st)(trace)(rest)
-						| _, _ -> (evaluate(stack)("Panic" :: trace)([])))
-					| [] -> None
-				))
-			| And -> (if array_len stack < 2 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| i :: j :: st -> 
-						(match i, j with
-						| (Bool i, Bool j) -> evaluate( Bool (i && j) :: st)(trace)(rest)
-						| _, _ -> (evaluate(stack)("Panic" :: trace)([])))
-					| [] -> None
-				))
-			| Or -> (if array_len stack < 2 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| i :: j :: st -> 
-						(match i, j with
-						| (Bool i, Bool j) -> evaluate( Bool (i || j) :: st)(trace)(rest)
-						| _, _ -> (evaluate(stack)("Panic" :: trace)([])))
-					| [] -> None
-				))
-			| Not -> (if array_len stack < 1 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| i :: st -> 
-						(match i with
-						| Bool i -> evaluate( Bool (not i) :: st)(trace)(rest)
-						| _ -> (evaluate(stack)("Panic" :: trace)([])))
-					| [] -> None
-				))
-			| Lt -> (if array_len stack < 2 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| i :: j :: st -> 
-						(match i, j with
-						| (Int i, Int j) -> evaluate( Bool (i < j) :: st)(trace)(rest)
-						| _, _ -> (evaluate(stack)("Panic" :: trace)([])))
-					| [] -> None
-				))
-			| Gt -> (if array_len stack < 2 then
-				(evaluate(stack)("Panic" :: trace)([])) else (
-					match stack with
-					| i :: j :: st -> 
-						(match i, j with
-						| (Int i, Int j) -> evaluate( Bool (i > j) :: st)(trace)(rest)
-						| _, _ -> (evaluate(stack)("Panic" :: trace)([])))
-					| [] -> None
-				))
-			)
-		| [] -> Some trace
-		in
-	match string_parse_c (parse_prog []) s with 
-	| Some (e, []) -> evaluate([])([])(e) 
-	| _ -> None  
-
+	let rec evaluate stack trace prog =
+      let array_len = List.length in
+      let panic_trace = evaluate stack ("Panic" :: trace) [] in
+    
+      match prog with
+      | Push ct :: rest -> evaluate (ct :: stack) trace rest
+      | Pop :: rest ->
+        (match stack with
+        | _ :: st -> evaluate st trace rest
+        | [] -> None)
+      | Trace :: rest ->
+        (match stack with
+        | top :: st -> evaluate (Unit () :: st) (const_to_string top :: trace) rest
+        | [] -> None)
+      | Add :: rest | Sub :: rest | Mul :: rest | Div :: rest ->
+        (match stack with
+        | i :: j :: st ->
+          (match i, j with
+          | Int x, Int y ->
+            let result =
+              match prog with
+              | Add :: _ -> Int (x + y)
+              | Sub :: _ -> Int (x - y)
+              | Mul :: _ -> Int (x * y)
+              | Div :: _ when y = 0 -> panic_trace
+              | Div :: _ -> Int (x / y)
+              | _ -> failwith "Unexpected operator"
+            in
+            evaluate (result :: st) trace rest
+          | _ -> panic_trace)
+        | [] -> None)
+      | And :: rest | Or :: rest ->
+        (match stack with
+        | Bool x :: Bool y :: st ->
+          let result =
+            match prog with
+            | And :: _ -> Bool (x && y)
+            | Or :: _ -> Bool (x || y)
+            | _ -> failwith "Unexpected operator"
+          in
+          evaluate (result :: st) trace rest
+        | _ -> panic_trace)
+      | Not :: rest ->
+        (match stack with
+        | Bool x :: st -> evaluate (Bool (not x) :: st) trace rest
+        | _ -> panic_trace)
+      | Lt :: rest | Gt :: rest ->
+        (match stack with
+        | Int x :: Int y :: st ->
+          let result =
+            match prog with
+            | Lt :: _ -> Bool (x < y)
+            | Gt :: _ -> Bool (x > y)
+            | _ -> failwith "Unexpected operator"
+          in
+          evaluate (result :: st) trace rest
+        | _ -> panic_trace)
+      | _ :: _ -> panic_trace
+      | [] -> Some trace
+    
