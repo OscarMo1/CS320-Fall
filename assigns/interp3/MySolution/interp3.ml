0786ada5b1e11f1fe64a29f392223787cc3b2548
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-12-11 20:08:02 -0500

    interp3

--- assigns/interp3/MySolution/interp3.ml
+++ assigns/interp3/MySolution/interp3.ml
@@ -329,36 +329,10 @@ let parse_prog (s : string) : expr =
   | _ -> raise SyntaxError
 
 
-(* compile helper functions *)
-
-let int2str(i0: int): string =
-   (*get_digit returns the individual digit at specified index of number*)
-   let getDigit(x: int): int =
-   let rec getDigitHelper(x: int)(result: int): int =
-      if x = 0 then result
-      else getDigitHelper (x - 1) (result * 10)
-   in
-   getDigitHelper x 1 in
-      
-   let length = num_length (abs i0) in
-   let isNeg = i0 < 0 in
-   (*create string with length based on length and isNeg*)
-   string_init (length + (if isNeg then 1 else 0))
-   (* lambda function that calls index of the value of int and turns into string*)
-   (fun i -> 
-      if i = 0 && isNeg then '-'
-      else
-         let index = length - (if isNeg then (i - 1) else i) - 1 in
-         let digit = abs (i0 / getDigit index) mod 10
-      in
-      chr(ord '0' + digit)
-      );;
-
-
 let compile (s : string) : string = (* YOUR CODE *)
   let push_expr(expr): string = 
     match expr with
-    | Int n -> int2str n
+    | Int n -> str_of_int n
     | Bool b -> if b then "True" else "False"
     | Var s -> s
     | Unit -> "Unit"
